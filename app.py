86c6f90165df1dcf4c29ce82137721267aad519c
davbk <davidc828@nycstudents.net>
2025-05-09 10:04:07 -0400

    e
    e

--- app.py
+++ app.py
@@ -10,27 +10,25 @@ def index():
 
     items = []
     for item in data:
-        items.append({
-            'id': item['id'],
-            'name': item['displayName'],
-            'type': item['type'],
-            'stackSize': item.get('stackSize', 64) 
-        })
+        if 'id' in item and 'displayName' in item and 'type' in item:
+            items.append({
+                'id': item['id'],
+                'name': item['displayName'],
+                'type': item['type'],
+                'stackSize': item.get('stackSize', 64)
+            })
 
     return render_template("index.html", items=items)
-
 
 @app.route("/item/<int:item_id>")
 def item_detail(item_id):
     response = requests.get("http://minecraft-ids.grahamedgecombe.com/items.json")
     data = response.json()
-
-    item = next((i for i in data if i['id'] == item_id), None)
+    item = next((i for i in data if i.get('id') == item_id), None)
     if not item:
         return "Item not found", 404
 
     return render_template("item.html", item=item)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
