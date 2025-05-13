32ee0e604e144962eef3c7087407b467e7404800
davbk <davidc828@nycstudents.net>
2025-05-13 09:56:10 -0400

    e
    e

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def item_detail(uuid):
     if not item:
         return "Item not found", 404
 
-    return render_template("skinbundles.html", item=item)
+    return render_template("skin.html", item=item)
 
 if __name__ == '__main__':
     app.run(debug=True)
